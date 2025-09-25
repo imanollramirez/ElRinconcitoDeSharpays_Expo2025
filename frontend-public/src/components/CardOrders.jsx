3d5916f62c7388345296ebb671a8a0cfd4923591
german <gonzagerman924@gmail.com>
2025-09-25 13:20:35 -0600

    error solucionado vercel

--- frontend-public/src/components/CardOrders.jsx
+++ frontend-public/src/components/CardOrders.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import orderslogo from "../assets/orderslogoprofile.png";
 import useOrdersWithCategories from "../components/order/hook/useOrders";
-import "../styles/CardOrder s.css";
+import "../styles/CardOrders.css";
 
 const CardOrders = () => {
   const { orders, loading, error, updateOrder, refresh } = useOrdersWithCategories();
